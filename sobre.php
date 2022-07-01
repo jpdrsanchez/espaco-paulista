acbaf3949eb374791f15861cd0fe4098602216c3
jpdrsanchez <joaopedrosanchez302@gmail.com>
2022-07-01 14:50:53 -0300

    feat: finalize about page

--- sobre.php
+++ sobre.php
@@ -12,6 +12,51 @@
     $coverText = 'Com mais de 20 anos de experiência, o Buffet Espaço Paulista nasce a partir de um conceito de preciosidade nos detalhes de cada passo da produção dos eventos! Possuímos uma equipe altamente especializada, que está pronta para oferecer as melhores soluções para o seu evento.';
   ?>
   <?php require_once './includes/page-cover.php'; ?>
+  <section class="aboutIntro">
+    <div class="container aboutIntro__container">
+      <h3 class="preTitle aboutIntro__preTitle">Como a nossa</h3>
+      <h1 class="siteTitle aboutIntro__title">História Começou</h1>
+      <p class="siteText aboutIntro__text">Somos movidos por emoções e sentimentos. Produzir desejos e compartilhar momentos de ternura em cada celebração é o nosso principal motor. Por isso, valorizamos profundamente a realização dos sonhos de nossos clientes e fazemos tudo com muita dedicação e carinho em todos os detalhes, para que os eventos celebrados aqui sejam feitos sempre de forma singular. Afinal de contas, sabemos quão especial é a realização dos sonhos de cada um que visita nosso Espaço.</p>
+      <a href="./contato.php" class="siteButton siteButton--black aboutIntro__button">
+        <svg width="40" height="53" viewBox="0 0 40 53" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M14.9714 28.1539V43.6471C14.9714 46.9753 15.3282 49.0401 16.0552 49.8416C17.0313 50.9895 18.5123 51.56 20.4914 51.56H22.4975V53H0V51.56H1.97913C4.19387 51.56 5.79603 50.8265 6.7452 49.3729C7.25682 48.5714 7.52609 46.656 7.52609 43.6471V9.35294C7.52609 6.02473 7.18277 3.95989 6.48267 3.1584C5.46617 2.01051 3.97173 1.43996 1.97913 1.43996H0V0H19.246C23.9448 0 27.6405 0.495835 30.3601 1.46713C33.0663 2.43842 35.3484 4.08894 37.2131 6.41189C39.0778 8.72805 40 11.4721 40 14.6441C40 18.9708 38.5863 22.4892 35.759 25.1925C32.9317 27.8958 28.9465 29.2543 23.7832 29.2543C22.5177 29.2543 21.1579 29.1592 19.6903 28.9758C18.2161 28.806 16.6476 28.5207 14.9714 28.1539ZM14.9714 25.9125C16.3312 26.1774 17.5429 26.3811 18.5998 26.4966C19.6432 26.6324 20.552 26.6936 21.2925 26.6936C23.9717 26.6936 26.2673 25.6408 28.2127 23.5488C30.1515 21.4567 31.1276 18.7534 31.1276 15.4388C31.1276 13.1498 30.6631 11.017 29.7408 9.06087C28.8186 7.1047 27.4992 5.63757 25.8095 4.66628C24.1064 3.69499 22.1811 3.19915 20.0269 3.19915C18.7075 3.19915 17.0313 3.45047 14.9714 3.9463V25.9125Z" fill="#111"></path>
+        </svg>
+        <span>Solicitar Orçamento</span>
+        <svg width="56" height="53" viewBox="0 0 56 53" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M16.986 32.9066L12.261 43.8334C11.3953 45.8395 10.9456 47.3473 10.9456 48.3436C10.9456 49.1275 11.3281 49.8316 12.0933 50.4294C12.8449 51.0139 14.4893 51.4058 17.0061 51.5918V53H0.502441V51.5918C2.6904 51.2132 4.09311 50.7216 4.73742 50.1039C6.05288 48.8817 7.50256 46.3908 9.09319 42.6511L27.5096 0H28.8117L47.0134 43.1028C48.4765 46.5701 49.8054 48.8086 51 49.8515C52.1947 50.8811 53.8591 51.4523 56.0001 51.5852V52.9934H35.3621V51.5852C37.4427 51.4789 38.8521 51.1401 39.5904 50.5556C40.3287 49.971 40.6844 49.2537 40.6844 48.4034C40.6844 47.2809 40.1609 45.514 39.1474 43.0895L34.8722 32.9L26.0398 11.9962L16.986 32.9066Z" fill="#111"></path>
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M12.6111 37.3612C15.8683 33.2212 20.6562 28.5827 33.3477 22.5532V24.6067C18.8422 31.7253 13.0832 42.4262 9.56299 49.617L12.6111 37.3612Z" fill="#111"></path>
+        </svg>
+      </a>
+    </div>
+    <div class="aboutIntro__backgroud">
+      <div class="container">
+        <img src="./assets/images/about/about-intro.jpg" alt="Espaço Paulista">
+      </div>
+    </div>
+  </section>
+  <section class="aboutStory">
+    <div class="container aboutStory__container">
+      <div class="aboutStory__images">
+        <img src="./assets/images/about/about-1.jpg" alt="Espaço Paulista" class="aboutStory__imageOne">
+        <img src="./assets/images/about/about-2.jpg" alt="Espaço Paulista" class="aboutStory__imageTwo">
+      </div>
+      <div class="aboutStory__content">
+        <h3 class="preTitle aboutStory__preTitle">Como a nossa</h3>
+        <h1 class="siteTitle aboutStory__title">História Começou</h1>
+        <p class="siteText aboutStory__text">Somos movidos pela satisfação dos nossos clientes e convidados. Para que, cada pessoa que entre em nosso espaço, sinta uma conexão profunda em todos os ambientes. Por isso, transportamos segurança e confiabilidade ao longo de décadas para inúmeros clientes e fornecedores. Viva o melhor dia de sua vida no Buffet Espaço Paulista!</p>
+        <a href="./contato.php" class="siteButton siteButton--black aboutStory__button">
+          <svg width="40" height="53" viewBox="0 0 40 53" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M14.9714 28.1539V43.6471C14.9714 46.9753 15.3282 49.0401 16.0552 49.8416C17.0313 50.9895 18.5123 51.56 20.4914 51.56H22.4975V53H0V51.56H1.97913C4.19387 51.56 5.79603 50.8265 6.7452 49.3729C7.25682 48.5714 7.52609 46.656 7.52609 43.6471V9.35294C7.52609 6.02473 7.18277 3.95989 6.48267 3.1584C5.46617 2.01051 3.97173 1.43996 1.97913 1.43996H0V0H19.246C23.9448 0 27.6405 0.495835 30.3601 1.46713C33.0663 2.43842 35.3484 4.08894 37.2131 6.41189C39.0778 8.72805 40 11.4721 40 14.6441C40 18.9708 38.5863 22.4892 35.759 25.1925C32.9317 27.8958 28.9465 29.2543 23.7832 29.2543C22.5177 29.2543 21.1579 29.1592 19.6903 28.9758C18.2161 28.806 16.6476 28.5207 14.9714 28.1539ZM14.9714 25.9125C16.3312 26.1774 17.5429 26.3811 18.5998 26.4966C19.6432 26.6324 20.552 26.6936 21.2925 26.6936C23.9717 26.6936 26.2673 25.6408 28.2127 23.5488C30.1515 21.4567 31.1276 18.7534 31.1276 15.4388C31.1276 13.1498 30.6631 11.017 29.7408 9.06087C28.8186 7.1047 27.4992 5.63757 25.8095 4.66628C24.1064 3.69499 22.1811 3.19915 20.0269 3.19915C18.7075 3.19915 17.0313 3.45047 14.9714 3.9463V25.9125Z" fill="#111"></path>
+          </svg>
+          <span>Solicitar Orçamento</span>
+          <svg width="56" height="53" viewBox="0 0 56 53" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M16.986 32.9066L12.261 43.8334C11.3953 45.8395 10.9456 47.3473 10.9456 48.3436C10.9456 49.1275 11.3281 49.8316 12.0933 50.4294C12.8449 51.0139 14.4893 51.4058 17.0061 51.5918V53H0.502441V51.5918C2.6904 51.2132 4.09311 50.7216 4.73742 50.1039C6.05288 48.8817 7.50256 46.3908 9.09319 42.6511L27.5096 0H28.8117L47.0134 43.1028C48.4765 46.5701 49.8054 48.8086 51 49.8515C52.1947 50.8811 53.8591 51.4523 56.0001 51.5852V52.9934H35.3621V51.5852C37.4427 51.4789 38.8521 51.1401 39.5904 50.5556C40.3287 49.971 40.6844 49.2537 40.6844 48.4034C40.6844 47.2809 40.1609 45.514 39.1474 43.0895L34.8722 32.9L26.0398 11.9962L16.986 32.9066Z" fill="#111"></path>
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M12.6111 37.3612C15.8683 33.2212 20.6562 28.5827 33.3477 22.5532V24.6067C18.8422 31.7253 13.0832 42.4262 9.56299 49.617L12.6111 37.3612Z" fill="#111"></path>
+          </svg>
+        </a>
+      </div>
+    </div>
+  </section>
   <?php require_once './includes/page-cta.php'; ?>
   <?php require_once './includes/footer.php'; ?>
 </body>
