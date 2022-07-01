c6ee0801e9de4fe05293c952674befccb2633460
jpdrsanchez <joaopedrosanchez302@gmail.com>
2022-07-01 18:35:42 -0300

    feat: finalize gallery component

--- espaco.php
+++ espaco.php
@@ -12,6 +12,91 @@
     $coverText = 'Oferecemos 2 amplos ambientes que possuem uma enorme estrutura e são únicos quando se fala em elegância. Levamos um clima majestoso para o seu evento, com uma arquitetura imponente e capacidade para acolher 300 convidados, levando toda a sofisticação que sua celebração merece. Quando falamos em inovação, modernidade e luxo, o Buffet Espaço Paulista é referência.';
   ?>
   <?php require_once './includes/page-cover.php'; ?>
+  <section class="spaceIntro">
+    <img src="./assets/images/space/space-intro.jpg" alt="Espaço Paulista" class="spaceIntro__image">
+    <div class="container spaceIntro__container">
+      <div class="spaceIntro__content">
+        <h3 class="preTitle spaceIntro__preTitle">Encate-se já</h3>
+        <h2 class="siteTitle spaceIntro__title">Na Fachada</h2>
+        <img src="./assets/images/space/space-intro.jpg" alt="Espaço Paulista" class="spaceIntro__textImage">
+        <p class="siteText spaceIntro__text">A fachada do Buffet Espaço Paulista arranca suspiros e conduz desejo para todos. Um formato moderno e totalmente reestruturado, que causará o devido impacto logo na entrada dos seus convidados.<br /><br />Levamos também todo o conceito de versatilidade para a fachada, fazendo com que sejam feitas diversas combinações e montagens, de acordo com o tema e decoração de cada evento, transportando ainda mais exclusividade para a sua celebração. Aqui, sua imaginação bate asas sem limites.</p>
+        <a href="./contato.php" class="siteButton siteButton--black spaceIntro__button">
+          <svg width="40" height="53" viewBox="0 0 40 53" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M14.9714 28.1539V43.6471C14.9714 46.9753 15.3282 49.0401 16.0552 49.8416C17.0313 50.9895 18.5123 51.56 20.4914 51.56H22.4975V53H0V51.56H1.97913C4.19387 51.56 5.79603 50.8265 6.7452 49.3729C7.25682 48.5714 7.52609 46.656 7.52609 43.6471V9.35294C7.52609 6.02473 7.18277 3.95989 6.48267 3.1584C5.46617 2.01051 3.97173 1.43996 1.97913 1.43996H0V0H19.246C23.9448 0 27.6405 0.495835 30.3601 1.46713C33.0663 2.43842 35.3484 4.08894 37.2131 6.41189C39.0778 8.72805 40 11.4721 40 14.6441C40 18.9708 38.5863 22.4892 35.759 25.1925C32.9317 27.8958 28.9465 29.2543 23.7832 29.2543C22.5177 29.2543 21.1579 29.1592 19.6903 28.9758C18.2161 28.806 16.6476 28.5207 14.9714 28.1539ZM14.9714 25.9125C16.3312 26.1774 17.5429 26.3811 18.5998 26.4966C19.6432 26.6324 20.552 26.6936 21.2925 26.6936C23.9717 26.6936 26.2673 25.6408 28.2127 23.5488C30.1515 21.4567 31.1276 18.7534 31.1276 15.4388C31.1276 13.1498 30.6631 11.017 29.7408 9.06087C28.8186 7.1047 27.4992 5.63757 25.8095 4.66628C24.1064 3.69499 22.1811 3.19915 20.0269 3.19915C18.7075 3.19915 17.0313 3.45047 14.9714 3.9463V25.9125Z" fill="#111"></path>
+          </svg>
+          <span>Solicitar Orçamento</span>
+          <svg width="56" height="53" viewBox="0 0 56 53" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M16.986 32.9066L12.261 43.8334C11.3953 45.8395 10.9456 47.3473 10.9456 48.3436C10.9456 49.1275 11.3281 49.8316 12.0933 50.4294C12.8449 51.0139 14.4893 51.4058 17.0061 51.5918V53H0.502441V51.5918C2.6904 51.2132 4.09311 50.7216 4.73742 50.1039C6.05288 48.8817 7.50256 46.3908 9.09319 42.6511L27.5096 0H28.8117L47.0134 43.1028C48.4765 46.5701 49.8054 48.8086 51 49.8515C52.1947 50.8811 53.8591 51.4523 56.0001 51.5852V52.9934H35.3621V51.5852C37.4427 51.4789 38.8521 51.1401 39.5904 50.5556C40.3287 49.971 40.6844 49.2537 40.6844 48.4034C40.6844 47.2809 40.1609 45.514 39.1474 43.0895L34.8722 32.9L26.0398 11.9962L16.986 32.9066Z" fill="#111"></path>
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M12.6111 37.3612C15.8683 33.2212 20.6562 28.5827 33.3477 22.5532V24.6067C18.8422 31.7253 13.0832 42.4262 9.56299 49.617L12.6111 37.3612Z" fill="#111"></path>
+          </svg>
+        </a>
+      </div>
+    </div>
+  </section>
+  <section class="spaceTent">
+    <div class="container spaceTent__container">
+      <h3 class="preTitle spaceTent__preTitle">A nossa</h3>
+      <h1 class="siteTitle spaceTent__title">Tenda</h1>
+      <p class="siteText spaceTent__text">A nossa exuberante Tenda irá extrair de seus convidados todas as sensações e emoções possíveis. Acompanhada do Lustre, ao estilo da francesa Baccarat, ao centro da passarela, formando o cenário ideal para diversas fotos. Na Tenda, você terá um ambiente de aconchego e beleza, além de utilizá-la como desejar. Será possível fazer desde cerimônias graciosas e emocionantes, até ambientes sérios, sofisticados e elegantes, para recepção de convidados. Ela é ainda, cercada por um magnífico jardim de inverno que irá certamente arrancar suspiros!</p>
+      <a href="./contato.php" class="siteButton siteButton--black spaceTent__button">
+        <svg width="40" height="53" viewBox="0 0 40 53" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M14.9714 28.1539V43.6471C14.9714 46.9753 15.3282 49.0401 16.0552 49.8416C17.0313 50.9895 18.5123 51.56 20.4914 51.56H22.4975V53H0V51.56H1.97913C4.19387 51.56 5.79603 50.8265 6.7452 49.3729C7.25682 48.5714 7.52609 46.656 7.52609 43.6471V9.35294C7.52609 6.02473 7.18277 3.95989 6.48267 3.1584C5.46617 2.01051 3.97173 1.43996 1.97913 1.43996H0V0H19.246C23.9448 0 27.6405 0.495835 30.3601 1.46713C33.0663 2.43842 35.3484 4.08894 37.2131 6.41189C39.0778 8.72805 40 11.4721 40 14.6441C40 18.9708 38.5863 22.4892 35.759 25.1925C32.9317 27.8958 28.9465 29.2543 23.7832 29.2543C22.5177 29.2543 21.1579 29.1592 19.6903 28.9758C18.2161 28.806 16.6476 28.5207 14.9714 28.1539ZM14.9714 25.9125C16.3312 26.1774 17.5429 26.3811 18.5998 26.4966C19.6432 26.6324 20.552 26.6936 21.2925 26.6936C23.9717 26.6936 26.2673 25.6408 28.2127 23.5488C30.1515 21.4567 31.1276 18.7534 31.1276 15.4388C31.1276 13.1498 30.6631 11.017 29.7408 9.06087C28.8186 7.1047 27.4992 5.63757 25.8095 4.66628C24.1064 3.69499 22.1811 3.19915 20.0269 3.19915C18.7075 3.19915 17.0313 3.45047 14.9714 3.9463V25.9125Z" fill="#111"></path>
+        </svg>
+        <span>Solicitar Orçamento</span>
+        <svg width="56" height="53" viewBox="0 0 56 53" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M16.986 32.9066L12.261 43.8334C11.3953 45.8395 10.9456 47.3473 10.9456 48.3436C10.9456 49.1275 11.3281 49.8316 12.0933 50.4294C12.8449 51.0139 14.4893 51.4058 17.0061 51.5918V53H0.502441V51.5918C2.6904 51.2132 4.09311 50.7216 4.73742 50.1039C6.05288 48.8817 7.50256 46.3908 9.09319 42.6511L27.5096 0H28.8117L47.0134 43.1028C48.4765 46.5701 49.8054 48.8086 51 49.8515C52.1947 50.8811 53.8591 51.4523 56.0001 51.5852V52.9934H35.3621V51.5852C37.4427 51.4789 38.8521 51.1401 39.5904 50.5556C40.3287 49.971 40.6844 49.2537 40.6844 48.4034C40.6844 47.2809 40.1609 45.514 39.1474 43.0895L34.8722 32.9L26.0398 11.9962L16.986 32.9066Z" fill="#111"></path>
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M12.6111 37.3612C15.8683 33.2212 20.6562 28.5827 33.3477 22.5532V24.6067C18.8422 31.7253 13.0832 42.4262 9.56299 49.617L12.6111 37.3612Z" fill="#111"></path>
+        </svg>
+      </a>
+    </div>
+    <div class="spaceTent__images">
+      <img src="./assets/images/space/space-1.jpg" alt="Espaço Paulista">
+      <img src="./assets/images/space/space-2.jpg" alt="Espaço Paulista">
+      <img src="./assets/images/space/space-3.jpg" alt="Espaço Paulista">
+    </div>
+  </section>
+  <section class="spaceHall">
+    <div class="container spaceHall__container">
+      <div class="spaceHall__content">
+        <h3 class="preTitle spaceHall__preTitle">Salão nobre e</h3>
+        <h1 class="siteTitle spaceHall__title">Projeção Mapeada</h1>
+        <img src="./assets/images/space/hall-1.jpg" alt="Espaço Paulista" class="spaceHall__textImage">
+        <p class="siteText spaceHall__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Condimentum sociis erat urna pharetra est. Vel enim odio elit sodales volutpat. Dignissim curabitur quis ut odio. Fringilla nisl, sed pharetra magnis vestibulum, nulla rhoncus, arcu. Libero tristique rutrum eget posuere. Vitae risus pellentesque pretium orci ullamcorper. Faucibus id quis in dapibus a in tincidunt magna morbi. Pharetra purus vel a semper malesuada facilisis eget. Imperdiet sagittis, tristique integer odio faucibus cursus sed mattis dolor. Malesuada rhoncus ipsum arcu.</p>
+        <a href="./contato.php" class="siteButton siteButton--black spaceHall__button">
+          <svg width="40" height="53" viewBox="0 0 40 53" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M14.9714 28.1539V43.6471C14.9714 46.9753 15.3282 49.0401 16.0552 49.8416C17.0313 50.9895 18.5123 51.56 20.4914 51.56H22.4975V53H0V51.56H1.97913C4.19387 51.56 5.79603 50.8265 6.7452 49.3729C7.25682 48.5714 7.52609 46.656 7.52609 43.6471V9.35294C7.52609 6.02473 7.18277 3.95989 6.48267 3.1584C5.46617 2.01051 3.97173 1.43996 1.97913 1.43996H0V0H19.246C23.9448 0 27.6405 0.495835 30.3601 1.46713C33.0663 2.43842 35.3484 4.08894 37.2131 6.41189C39.0778 8.72805 40 11.4721 40 14.6441C40 18.9708 38.5863 22.4892 35.759 25.1925C32.9317 27.8958 28.9465 29.2543 23.7832 29.2543C22.5177 29.2543 21.1579 29.1592 19.6903 28.9758C18.2161 28.806 16.6476 28.5207 14.9714 28.1539ZM14.9714 25.9125C16.3312 26.1774 17.5429 26.3811 18.5998 26.4966C19.6432 26.6324 20.552 26.6936 21.2925 26.6936C23.9717 26.6936 26.2673 25.6408 28.2127 23.5488C30.1515 21.4567 31.1276 18.7534 31.1276 15.4388C31.1276 13.1498 30.6631 11.017 29.7408 9.06087C28.8186 7.1047 27.4992 5.63757 25.8095 4.66628C24.1064 3.69499 22.1811 3.19915 20.0269 3.19915C18.7075 3.19915 17.0313 3.45047 14.9714 3.9463V25.9125Z" fill="#111"></path>
+          </svg>
+          <span>Solicitar Orçamento</span>
+          <svg width="56" height="53" viewBox="0 0 56 53" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M16.986 32.9066L12.261 43.8334C11.3953 45.8395 10.9456 47.3473 10.9456 48.3436C10.9456 49.1275 11.3281 49.8316 12.0933 50.4294C12.8449 51.0139 14.4893 51.4058 17.0061 51.5918V53H0.502441V51.5918C2.6904 51.2132 4.09311 50.7216 4.73742 50.1039C6.05288 48.8817 7.50256 46.3908 9.09319 42.6511L27.5096 0H28.8117L47.0134 43.1028C48.4765 46.5701 49.8054 48.8086 51 49.8515C52.1947 50.8811 53.8591 51.4523 56.0001 51.5852V52.9934H35.3621V51.5852C37.4427 51.4789 38.8521 51.1401 39.5904 50.5556C40.3287 49.971 40.6844 49.2537 40.6844 48.4034C40.6844 47.2809 40.1609 45.514 39.1474 43.0895L34.8722 32.9L26.0398 11.9962L16.986 32.9066Z" fill="#111"></path>
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M12.6111 37.3612C15.8683 33.2212 20.6562 28.5827 33.3477 22.5532V24.6067C18.8422 31.7253 13.0832 42.4262 9.56299 49.617L12.6111 37.3612Z" fill="#111"></path>
+          </svg>
+        </a>
+      </div>
+      <div class="spaceHall__image">
+        <img src="./assets/images/space/hall-1.jpg" alt="Espaço Paulista">
+      </div>
+    </div>
+    <div class="spaceHall__backgroud">
+      <div class="container">
+        <img src="./assets/images/space/hall-2.jpg" alt="Espaço Paulista">
+      </div>
+    </div>
+  </section>
+  <?php
+    $gallerySubtitle = 'Galeria de fotos';
+    $galleryTitle = 'Do Nosso Espaço';
+    $galleryText = 'O que acha de realizar sua cerimônia neste lindo espaço? Confira a galeria de fotos do Buffet Espaço Paulista. Nós te ajudamos a realizar esse sonho em todos os detalhes! Você poderá personalizá-lo como desejar, durante todo o planejamento do seu evento.';
+    $images = [];
+    for($i = 1; $i <= 18; $i++) {
+      $images[] = [
+        'link' => './assets/images/galleries/space-gallery/img-' . $i . '.jpg',
+        'image' => './assets/images/galleries/space-gallery/img-' . $i . '.jpg',
+        'title' => 'Image' . $i,
+      ];
+    }
+  ?>
+  <?php require_once './includes/page-gallery.php'; ?>
   <?php require_once './includes/page-cta.php'; ?>
   <?php require_once './includes/footer.php'; ?>
 </body>
